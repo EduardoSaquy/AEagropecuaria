8d225f9193b04c330d000f197792086e5c795187
EduardoSaquy <eduardosaquy@gmail.com>
2026-08-18 15:10:01 -0300

    Create sw_lavoura.js

---
+++ sw_lavoura.js
@@ -0,0 +1,21 @@
+const CACHE = 'lavoura-v1';
+const SHELL = ['./AELavoura.html', './manifest_lavoura.json', './icon-lavoura-192.png', './icon-lavoura-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
